c26e70e1df2315d00c7e2783fcd898bca314559f
Vladimir Kirillov <proger@wilab.org.ua>
2013-03-01 22:57:16 +0200

    lldb-run: do not process empty commands

--- lldb-run.py
+++ lldb-run.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0, '/System/Library/PrivateFrameworks/LLDB.framework/Resources/Python')
 
 import os
+from itertools import ifilter
 import jinja2
 
 pid = int(sys.argv[1])
@@ -25,7 +26,7 @@ if pid > 0:
 
     sys.stderr.write('attach: {}\n'.format(errp))
 
-for command in filter(None, sys.stdin.readlines()):
+for command in ifilter(None, (l.strip() for l in sys.stdin.readlines())):
     comm = str(jinja2.Template(command).render(**os.environ))
 
     if pid > 0:
